f56ff3dc1116e2b3850fe886e9d9a7da01181e23
Maximo Nieva <maximonieva1000@gmail.com>
2025-10-15 01:52:37 -0300

    feat(payments): add Mercado Pago webhook with HMAC verification, idempotency, and atomic updates; extend Payment model and add WebhookLog

--- resqfood/urls.py
+++ resqfood/urls.py
@@ -1,7 +1,7 @@
 # resqfood/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -14,10 +14,15 @@ urlpatterns = [
 
     # API (todo lo de DRF va debajo de /api/)
     path("api/", include("api.urls")),       # /api/packs/ -> JSON
+    path("api/payments/", include("payments.urls")),
 
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),   
     path('', include('marketplace.urls')),
+    path("webhooks/mercadopago/", include("payments.urls_webhooks")),
+
+    path("api/auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 if settings.DEBUG:
